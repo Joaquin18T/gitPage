1d55592b43e02fe1da9c5d803e3cb17d62dc3a73
Joaquin18T <torrescondo@933958993>
2025-01-30 18:15:16 -0500

    asdd

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ErrorPage() {
+  return (
+    <div>
+      ERROR DE RUTA
+    </div>
+  )
+}
--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Home'
 import About from '../About'
+import ErrorPage from '../ErrorPage'
 
 export default function Rutas() {
   return (
@@ -9,6 +10,7 @@ export default function Rutas() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
       </Routes>
     </>
   )
